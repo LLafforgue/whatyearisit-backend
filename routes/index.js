93f49d0a81c4ec11b99dfc4fa45b559335d5556c
Ludovic.lafforgue.c@gmail.com <ludovic.lafforgue.c@gmail.com>
2025-06-27 10:51:34 +0200

    premier deploiement3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/year', function(req, res) {
-  res.json({'year': new Date().getUTCFullYear() });
+  res.json({'year': new Date().getUTCFullYear().toString()});
 });
 
 module.exports = router;
